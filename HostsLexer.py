d6a9b1d12889f922a619c58d56c81f31721f0ddd
qingshan zhuan <qszhuan@yaya.local>
2017-05-30 23:44:36 +1000

    add ipv6 lexer

--- HostsLexer.py
+++ HostsLexer.py
@@ -12,6 +12,7 @@ class HostsLexer(RegexLexer):
         'root': [
             (r'#.*?$', Name.Decorator),
             (r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', Name.Class),
+            (r'[A-Fa-f0-9]{0,4}:{1,2}[A-Fa-f0-9]{1,4}%?\w*\b', Name.Class),
             (r'.+', Name.Attribute)
         ]
     }
